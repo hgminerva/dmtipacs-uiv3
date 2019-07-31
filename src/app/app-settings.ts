dc21ee0a53fe62c7467597993aef0d83d12e26b4
NoahOliverRIgonan <oliverrigonan@gmail.com>
2019-07-31 14:57:16 +0800

    Change App Settings File - API URL

--- src/app/app-settings.ts
+++ src/app/app-settings.ts
@@ -1,4 +1,5 @@
 export class AppSettings {
-    public defaultAPIHostURL: string = "http://localhost:52125";
+    // public defaultAPIHostURL: string = "http://localhost:52125";
+    public defaultAPIHostURL: string = "http://dmtipacsv3-api.azurewebsites.net";
     // more settings ...
 }
